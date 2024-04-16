510d740ed36108704d7a4082bd8d18cdbdf149bf
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2024-04-16 12:33:25 -0300

    fixed warnings

--- module_home/lib/presenter/pages/home_page.dart
+++ module_home/lib/presenter/pages/home_page.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
--- module_home/test/module_home_test.dart
+++ module_home/test/module_home_test.dart
@@ -1,5 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:module_home/module_home.dart';
-
 void main() {}
